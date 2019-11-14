047f37ee3f7ac5d6e74de74e4466b80b61111945
Ariel Zapata <ariel.zapata.j@gmail.com>
2019-11-14 16:15:31 -0600

    exercise 2 refactor

---
+++ 2refactor.rb
@@ -0,0 +1,8 @@
+def longname
+  name = fullname
+  return unless organization_id
+  organization = Organization.lookup(id: organization_id)
+  return unless organization
+  name += " (#{organization.name})"
+  name
+end
