9465d6b2a12678f83c9e34619c58cc0a82d35c28
Ariel Zapata <ariel.zapata.j@gmail.com>
2019-11-15 16:23:35 -0600

    Correction

--- numbersum.rb
+++ numbersum.rb
@@ -1,14 +1,15 @@
-def numbers(numbers_array)
-  sum=0
-  numbers_array.each do |current_element|
-    current_element = current_element.to_s
-    numbers_reverse=current_element.reverse
-    numbers_index=current_element.reverse.index("0")
-      
-    sum+=numbers_reverse.slice(0..numbers_index-1).reverse.to_i if current_element.reverse.byteslice(0).to_i!=0
+def sum_numbers(numbers)
+    sum = 0
+  
+  numbers.each do |number|
+    number = number.to_s
+    numbers_reverse = number.reverse
+    number_index = number.reverse.index("0")-1
+    sum += numbers_reverse.slice(0..number_index).reverse.to_i if numbers_reverse.byteslice(0).to_i != 0
   end
+  
   sum  
 end
 
-numbers_array = [1200000265, 1000002360, 1670000800, 1000006002, 1000000233]
-numbers(numbers_array)
+numbers = [1200000265, 1000002360, 1670000800, 1000006002, 1000000233]
+sum_numbers(numbers)
