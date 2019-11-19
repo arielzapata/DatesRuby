17a3e2080d93335f4c9cdc6f5e6eff2b676c9adf
Ariel Zapata <ariel.zapata.j@gmail.com>
2019-11-19 17:23:12 -0600

    Exercise auto trip

---
+++ auto.rb
@@ -0,0 +1,36 @@
+class Auto
+  attr_accessor :model, :color, :year, :car_raise, :trip_raise
+
+  def initialize
+    @car_raise = car_raise
+    @trip_raise = trip_raise
+    self.car_raise = true
+    self.trip_raise = true
+  end
+
+  def start_car
+    puts "the car was started"
+    self.car_raise=false
+  end
+
+  def start_trip
+    begin 
+      self.trip_raise=false
+      raise "Trip can't start if car has not been started" if self.car_raise
+      puts "The trip has been started"
+    end  
+  end
+
+  def finish_trip
+    begin
+      raise "Trip can't finish if never started it" if self.trip_raise
+      puts "The trip has been finished"
+    end
+  end
+end
+
+start = Auto.new
+
+start.start_car
+start.start_trip
+start.finish_trip
