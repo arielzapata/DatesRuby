7c2f223de0f8e4f41c5602c8283dd70396208584
Ariel Zapata <ariel.zapata.j@gmail.com>
2019-11-19 11:36:15 -0600

    method name modified

--- user.rb
+++ user.rb
@@ -1,15 +1,15 @@
 class User
   attr_accessor :name, :lastname
-  
+
   def initialize(name, lastname)
     @name = name
     @lastname = lastname
   end
 
-  def complete_name
-  "#{name} #{lastname}"
+  def name
+    "#{name} #{lastname}"
   end
 end
 
-name=User.new("Ariel", "Zapata")
-name.complete_name
+user=User.new("Ariel", "Zapata")
+user.name
