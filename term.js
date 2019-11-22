31b5d185869c726ce1152dcb1e597d26d9d01a49
arielzapata <45351266+arielzapata@users.noreply.github.com>
2019-11-22 13:20:13 -0600

    Update term.js

--- term.js
+++ term.js
@@ -2,7 +2,7 @@ const people = ["Pedro", "Maria", "Rosa"]
 const animals = ["Caballo", "Oso", "Perro"]
 const things = ["Petroleo", "Carne", "Rosca"]
 
-const term = (findTerm) => {
+const findTerm = (term) => {
   const word = people.concat(animals).concat(things)
   const matchs = word.filter(word => word.toLowerCase().search(term.toLowerCase()) > -1)
   return matchs 
