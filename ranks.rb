3f90f12cfb43a298e2fc7dc20430bc07dfe72650
arielzapata <45351266+arielzapata@users.noreply.github.com>
2019-12-06 17:40:55 -0600

    Update ranks.rb

--- ranks.rb
+++ ranks.rb
@@ -2,8 +2,8 @@ ranks = [[40, 85], [10, 60], [70, 160]]
 intersections_counter = 0
 intersections = []
 
-ranks.each do |number,position|
-  ranks.each_with_index do |rank, index| 
+ranks.each do |number, rank_position|
+  ranks.each_with_index do |rank, rank_position| 
     if number.between?(rank[0], rank[1])
       intersections_counter += 1
     end  
