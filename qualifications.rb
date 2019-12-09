81a70ecb5248caaf7130edf1a7724021fc57b4ee
Ariel Zapata <ariel.zapata.j@gmail.com>
2019-12-09 11:30:42 -0600

    asdasd

--- qualifications.rb
+++ qualifications.rb
@@ -3,5 +3,5 @@ def verify_qualification(verdict, qualification)
   return "no existe veredicto para el valor"
 end 
   
-verdict = { sinfico: 0, con_poco_fico: 10, con_fico_regular: 20 }
+verdict = { sin_fico: 0, con_poco_fico: 10, con_fico_regular: 20 }
 verify_qualification(verdict, 3)   
