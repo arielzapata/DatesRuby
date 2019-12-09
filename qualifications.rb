9e17b590f8ee783bea55d1a56da4a6f5a02128d2
Ariel Zapata <ariel.zapata.j@gmail.com>
2019-12-09 11:24:15 -0600

    Exercise 2 TEST

---
+++ qualifications.rb
@@ -0,0 +1,7 @@
+def verify_qualification(verdict, qualification)
+  verdict.select { |k, v| return k.to_s.sub('_', ' ') if qualification < v }
+  return "no existe veredicto para el valor"
+end 
+  
+verdict = { sinfico: 0, con_poco_fico: 10, con_fico_regular: 20 }
+verify_qualification(verdict, 3)  
